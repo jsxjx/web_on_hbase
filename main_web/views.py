a6397259eb4adba4f418f420cb2e729424668413
waterwoodwind <469798015@qq.com>
2016-01-27 09:03:32 +0800

    加入存入模块的开头

--- main_web/views.py
+++ main_web/views.py
@@ -5,3 +5,7 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+def storing_data(request):
+
+    return HttpResponse("已完成数据存入")
