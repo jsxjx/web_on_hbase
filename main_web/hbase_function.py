34db3a570d513585da727ac144241715f9e49f46
waterwoodwind <469798015@qq.com>
2016-01-28 14:10:14 +0800

    优化HBASE的接口模块

--- main_web/hbase_function.py
+++ main_web/hbase_function.py
@@ -9,26 +9,20 @@ class HBASE_interface():
         pass
 
     def connect_hbase(self):
-        print u"%s 连接数据库：" % time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()))
+        print u"%s 连接数据库" % time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()))
         connection = happybase.Connection(host='10.210.180.43',
                                          port=9090,
                                          timeout = None,
                                          autoconnect=True,
                                          compat='0.94',
                                          )
-        print u"%s 连接数据库完毕：" % time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()))
+        print u"%s 连接数据库完毕" % time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()))
+        return connection
         #print connection.tables()
 
     # 开始创建数据表
     def create_table(self, table_name):
-        print u"%s 连接数据库：" % time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()))
-        connection = happybase.Connection(host='10.210.180.43',
-                                         port=9090,
-                                         timeout = None,
-                                         autoconnect=True,
-                                         compat='0.94',
-                                         )
-        print u"%s 连接数据库完毕：" % time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()))
+        connection = self.connect_hbase()
         connection.create_table(
             table_name,
             {'c1': dict(max_versions=1, compression='GZ'),
@@ -36,3 +30,14 @@ class HBASE_interface():
              'c3': dict(max_versions=1, compression='GZ'),
             }
         )
+        print u"%s 已创建表：%s" % (time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time())), table_name)
+
+    def delete_table(self,table_name):
+        connection = self.connect_hbase()
+        connection.delete_table(table_name, True)
+        print u"%s 已删除表：%s" % (time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time())), table_name)
+
+
+hbase_interface = HBASE_interface()
+hbase_interface.create_table('mytable_no_COM')
+hbase_interface.delete_table('mytable_no_COM')
