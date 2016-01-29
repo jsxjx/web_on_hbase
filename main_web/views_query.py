20f2c943f27ad5859cadcaa627e8f49bf68a7ff3
waterwoodwind <469798015@qq.com>
2016-01-29 10:16:37 +0800

    删除单表查询页面打开就有数据的功能

--- main_web/views_query.py
+++ main_web/views_query.py
@@ -5,28 +5,8 @@ from django.http import HttpResponse
 from hbase_function import HBASE_interface
 import json
 
-def single_para_query(request):
-    hbase_interface = HBASE_interface()
-    if request.method == 'POST':
-        post_index = request.POST['firstname']
-    else:
-        post_index = 1
-
-    tablename = 'B-1527_20160125052953'
-    cf_name = 'c1:1328'
-    cf_set = ['c1:1328']
-    result_scan_dict = hbase_interface.query_table(tablename,cf_set)
-    result_list = []
-    for key, value in result_scan_dict.items():
-        single = {'index' : key, 'value' : value[cf_name]}
-        result_list.append(single)
-    result_json = json.dumps(result_list)
-    return HttpResponse(result_json)
-
 def ajax_single_para(request):
     hbase_interface = HBASE_interface()
-
-
     post_index = request.GET.get('column_index', None)
     index = post_index.encode('utf-8')
     tablename = 'B-1527_20160125052953'
