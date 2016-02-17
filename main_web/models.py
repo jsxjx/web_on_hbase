038aaf8c738c97f4c67f01e6197697aa1cc7db14
waterwoodwind <469798015@qq.com>
2016-02-17 15:08:27 +0800

    对译码模块优化，封装成类和函数，并在参数名加上ID号

--- main_web/models.py
+++ main_web/models.py
@@ -1,3 +1,51 @@
+# coding:utf-8
 from django.db import models
 
 # Create your models here.
+import happybase
+import time
+from hbase_function import HBASE_interface
+
+def save_decode_list_to_hbase(list_all_para_turn, file):
+    hbase_interface = HBASE_interface()
+    connection = happybase.Connection(host='10.210.180.43',
+                                     port=9090,
+                                     timeout = None,
+                                     autoconnect=True,
+                                     compat='0.94',
+                                     )
+    # 向索引表中插入数据分表的表头信息
+    table_tablename_index = connection.table("tablename_index")
+    pro_Aircraft_Identification = file[0:6]
+    pro_updata_Date = file[7:11] + '-' + file[11:13] + '-' + file[13:15]
+    pro_updata_Time = file[15:17] + ':' + file[17:19] + ':' + file[19:21]
+    dic_table_info = {'c1:Aircraft_Identification': pro_Aircraft_Identification,
+                      'c1:updata_Date': pro_updata_Date,
+                      'c1:updata_Time':pro_updata_Time}
+    table_tablename_index.put(file[0:21], dic_table_info)
+
+    #建立分表，并向分表中插入数据
+    hbase_interface.create_table(file[0:21])
+    table = connection.table(file[0:21])
+    print table
+    happybase_start_time = time.clock()
+    b = table.batch()
+    counter_list_all_para = len(list_all_para_turn)
+    counter_list_columns = len(list_all_para_turn[0])
+
+    # 按python list 标号从零开始改为从一开始，以符合数据库设计
+    counter_list_columns_app_1 = counter_list_columns + 1
+    for i in range(0, counter_list_all_para):
+        #print u"第 %s 行"%i
+        str_i = str(i).zfill(5)
+        dic_j = {}
+        for j in range(1, counter_list_columns_app_1):
+            str_value = str(list_all_para_turn[i][j-1]) #对应在list中的下标要减一
+            dic_j['c1:'+ str(j)] = str_value
+
+        table.put(str_i, dic_j)
+
+    #b.send()
+
+    happybase_end_time = time.clock()
+    print u"存入耗时： %s"%(happybase_end_time - happybase_start_time)
---
+++ main_web/second_storing.py
@@ -0,0 +1,77 @@
+#coding=utf-8
+
+from hbase_function import HBASE_interface
+from hbase_function import LIST_to_STR
+from aircraft_config import AC_WQAR_CONFIG
+
+class Second_Storing:
+
+    def __init__(self):
+        self.hb_if = HBASE_interface()
+        self.list_str = LIST_to_STR()
+        self.ac_config = AC_WQAR_CONFIG()
+        pass
+
+    def merge_GMT_time(self):
+        GMT_model_256 = [89, 90, 91]
+        GMT_model_512 = [96, 97, 98]
+        hb_if = self.hb_if
+        list_str = self.list_str
+        ac_config = self.ac_config
+        for table_name in hb_if.list_query_tablenames():
+            # 飞机构型判断
+            ac_number = table_name[0:6]
+            if ac_number in ac_config.WQAR256_SERISE_list:
+                model = GMT_model_256
+            elif ac_number in ac_config.WQAR512_SERISE_list:
+                model = GMT_model_512
+            else:
+                print '无此机号：' + table_name[0:6]
+                return
+            #连接数据库中的表
+            table = hb_if.table(table_name)
+            self.GMT_storing_to_c2(table_name, model)
+            print table_name, table
+
+
+    def GMT_storing_to_c2(self, table_name, model):
+        hb_if = self.hb_if
+        list_str = self.list_str
+        ac_config = self.ac_config
+        cf_set = hb_if.list_make_columns_family('c1', model)
+        table = hb_if.table(table_name)
+        result_scan_dict = hb_if.query_table(table_name,cf_set)
+        print result_scan_dict
+        result_list = []
+        para_name_dic = result_scan_dict['00000']
+        print para_name_dic
+        GMT_time = ''
+        list_GMT_time = []
+        for key, value in result_scan_dict.items():
+            #print key, value
+            if key == "00000":
+                list_GMT_time.append('$GMT TIME')
+                table.put(key, {'c2:1': '$GMT TIME'})
+                continue
+            if value[cf_set[0]] == '':
+                list_GMT_time.append(GMT_time)
+                table.put(key, {'c2:1': GMT_time})
+                continue
+
+            for key2, value2 in value.items():
+                value[key2] = (str(int(float(value2)))).zfill(2)
+            seq = ':'
+            GMT_time = seq.join([value[cf_set[0]], value[cf_set[1]], value[cf_set[2]]])
+            list_GMT_time.append(GMT_time)
+            table.put(key, {'c2:1': GMT_time})
+
+
+
+
+second_storing = Second_Storing()
+second_storing.merge_GMT_time()
+
+
+
+
+
