38dcb02e195d1931acfabcf17c0ef6c3d2a2338c
waterwoodwind <469798015@qq.com>
2016-02-26 15:48:46 +0800

    添加路径识别功能

--- main_web/views.py
+++ main_web/views.py
@@ -6,6 +6,7 @@ from decode_all_function import MERGE_DECODE_LIST
 from models import save_decode_list_to_hbase
 
 import time
+import socket
 
 def home(request):
     return render(request, 'home.html')
@@ -14,10 +15,14 @@ def storing_data(request):
 
     allstarttime = time.clock()
     decode_list = MERGE_DECODE_LIST()
-    #单位电脑路径
-    #dir_path = r'G:\QAR_DATA\for_test_wgl'
-    #测试路径
-    dir_path = r'G:\QAR_DATA\append_upload'
+
+    computer_name = socket.getfqdn(socket.gethostname())
+    if computer_name == "wxjd-61222177.cq.airchina.com.cn":
+        #单位电脑路径
+        dir_path = r'G:\QAR_DATA\append_upload'
+    else:
+        #服务器电脑路径
+        dir_path = '/opt/QAR_DATA/hbase_append_upload'
     decode_list.all_decode_list(dir_path, save_decode_list_to_hbase)
     #decode_list.all_decode_list(dir_path, decode_list.save_to_csv)
 
